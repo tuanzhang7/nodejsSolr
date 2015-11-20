1b200f3179c27bc0cc8da3807bc8b544e7df9442
tuanzhang <tuanzhang@gmail.com>
2015-11-20 17:44:07 +0800

    download with async.series and async.eachLimit succ

--- app.js
+++ app.js
@@ -8,68 +8,77 @@ var moment= require('moment');
 var logger=require('./log.js').logger;
 var fs = require('fs');
 
-var mimeType="text/html";
+//var mimeType="text/html";
+var mimeType="application/pdf";
 var startId=0;
 var size=100;
 var counter=0;
 var finished=false;
 
 var start=moment();
-
+var middle=moment();
 loop(startId);
 
 function loop(cursor) {
     repo.getNodeIdByMimeType(mimeType,cursor,size, function (data) {
-        //logger.info('data.length:'+data.length);
+        logger.info('startId:'+startId);
+        if(counter>1000){
+            stopWatch(start,counter);
+            return;
+        }
         if(data.length==0){
             stopWatch(start,counter);
             return;
         }else{
             startId=_.max(data);
-            //data.forEach(function (nodeId) {
+            async.series([
+                function(callback){
 
-            //});
-            //change to sync call to avoid server hang
-
-            async.eachLimit(data,2, function (nodeId,callback) {
-                //logger.info("process:"+nodeId);
-                var digit = (''+nodeId).slice(0,3);
-                var path="content/"+digit;
-                var fileName=path+"/"+nodeId+".txt";
-                fs.exists(fileName, function (exists) {
-                    if(exists){
-                        fs.readFile(fileName, function (err, data) {
-                            if (err) throw err;
-                            //logger.info("read from local."+nodeId);
-                            callback();
-                        });
-
-                    }
-                    else{
-                        contentHelper.getByNodeId(nodeId, function (data2) {
-                            if (fs.existsSync(path)===false) {
-                                fs.mkdirSync(path);
-                                fs.writeFile(fileName, data2, function(err) {
-                                    if(err) {
-                                        return logger.error(err);
-                                    }
+                    async.eachLimit(data,2, function (nodeId,callback) {
+                        //logger.info("process:"+nodeId);
+                        var digit = (''+nodeId).slice(0,3);
+                        var path="content/"+digit;
+                        var fileName=path+"/"+nodeId+".txt";
+                        fs.exists(fileName, function (exists) {
+                            if(exists){
+                                fs.readFile(fileName, function (err, data) {
+                                    if (err) throw err;
+                                    //logger.info("read from local."+nodeId);
+                                    callback();
                                 });
                             }
-                            //solrClient.updateContent(nodeId,data2);
-                            callback();
+                            else{
+                                contentHelper.getByNodeId(nodeId, function (data2) {
+                                    if (fs.existsSync(path)===false) {
+                                        fs.mkdirSync(path);
+                                    }
+                                    fs.writeFile(fileName, data2, function(err) {
+                                        //logger.info("save to local."+nodeId);
+                                        callback();
+                                        if(err) {
+                                            return logger.error(err);
+                                        }
+                                    });
+                                    //solrClient.updateContent(nodeId,data2);
+
+                                });
+                            }
                         });
-                    }
-                });
-                counter++;
-                //return;
-            }, function(done) {
-                //console.log("done");
+
+                    }, function(done) {
+                        //console.log("done");
+                        callback();
+                    });
+                }
+            ],
+            function(err, results){
+                counter+=size;
+                if(counter%1000==0){
+                    stopWatch(middle,1000);
+                    middle=moment().valueOf();
+                }
+                loop(startId);
             });
-            //if(counter>=1000){
-            //    stopWatch(start,counter);
-            //    return;
-            //}
-            loop(startId);
         }
     });
 }
